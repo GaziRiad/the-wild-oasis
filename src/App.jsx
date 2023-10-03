5487558794b70e61a920c8d7798002de7f8ff50b
GaziRiad <ouhibimustapha36@gmail.com>
2023-10-03 23:10:05 +0100

    Fetching cabins data from supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Dashboard from "./pages/Dashboard";
@@ -11,12 +12,12 @@ import Users from "./pages/Users";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyle";
 import AppLayout from "./ui/AppLayout";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 1000,
+      // staleTime: 60 * 1000,
+      staleTime: 0,
     },
   },
 });
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,25 +1,19 @@
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from ".././services/apiCabins";
-import { useEffect } from "react";
 
 function Cabins() {
-  useEffect(() => {
-    async function fetchCabins() {
-      const data = await getCabins();
-      console.log(data);
-    }
-    fetchCabins();
-  });
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://ujqgxkulrurehiurhjrd.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
-        height={450}
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
