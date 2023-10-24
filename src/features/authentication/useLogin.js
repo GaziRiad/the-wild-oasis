b1f5840c437e51f800b290d505c0353096274bb3
GaziRiad <ouhibimustapha36@gmail.com>
2023-10-24 17:33:26 +0100

    Authorization: Protecting routes

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,22 +1,26 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { isLoading, mutate: login } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       navigate("/dashboard");
+
+      // We don't need to check the session and then get the current user at this point since the user has just loged ikn
+      // so we store directly the user into the cache (Not really necessary just for better UX)
+      queryClient.setQueriesData(["user"], user);
     },
     onError: (err) => {
       console.log("Error", err);
       toast.error("Provided email or password are incorrect.");
     },
   });
-
   return { isLoading, login };
 }
 
